fe7aaea7cc66000b689f6bee40d6acc3db0adbf9
Toasa <atsushi.tohyama.160.333@gmail.com>
2020-09-22 23:19:11 +0900

    update

--- abc/157/d.py
+++ abc/157/d.py
@@ -2,6 +2,8 @@ class UFNode:
     def __init__(self, ID):
         self.ID = ID
         self.parent = None
+        # size of connected component
+        self.cc_size = 1
 
 class UFTree:
     def __init__(self, N):
@@ -27,28 +29,49 @@ class UFTree:
         if root1.ID == root2.ID:
             return
 
-        root1.parent = root2
-        
+        if root1.cc_size <= root2.cc_size:
+            root1.parent = root2
+            root2.cc_size += root1.cc_size
+        else:
+            root2.parent = root1
+            root1.cc_size += root2.cc_size
+
+    def get_cc_size(self, node):
+        root = self.get_root(node)
+        return root.cc_size
 
 def main():
     N, M, K = map(int, input().split())
 
-    friend_map = [ [False] * N for i in range(N) ]
-    block_map = [ [False] * N for i in range(N) ]
+    friend_list = [set([]) for i in range(N) ]
+    block_list = [set([]) for i in range (N)]
 
     tree = UFTree(N)
 
+    # friend
     for i in range(M):
         a, b = map(int, input().split())
         a, b = a - 1, b - 1
-        friend_map[a][b] = True
-        friend_map[b][a] = True
+        friend_list[a].add(b)
+        friend_list[b].add(a)
         tree.merge(tree.nodes[a], tree.nodes[b])
 
+    # blocked
     for i in range(K):
         a, b = map(int, input().split())
         a, b = a - 1, b - 1
-        block_map[a][b] = True
-        block_map[b][a] = True
+        if not tree.same(tree.nodes[a], tree.nodes[b]):
+            continue
+
+        # 同じ連結成分であれば後の排除処理のためにリストに追加する
+        block_list[a].add(b)
+        block_list[b].add(a)
+
+    result = ""
+    for i in range(N):
+        n = tree.nodes[i]
+        result += str(tree.get_cc_size(n) - (len(friend_list[i]) + len(block_list[i]) + 1)) + " "
+
+    print(result[:-1])
 
 main()
