250b179e1eeb4f1ba262fe3e74cacaf0dbca5dbe
Toasa <atsushi.tohyama.160.333@gmail.com>
2020-09-25 00:54:00 +0900

    update

---
+++ abc/155/d.py
@@ -0,0 +1,18 @@
+N, K = map(int, input().split())
+
+A = list(map(int, input().split()))
+A = sorted(A)
+
+if A[0] >= 0:
+    i = 0
+elif A[len(A)-1] < 0:
+    i = 
+
+
+
+A_minus = 
+
+
+print(result)
+    
+
--- abc/179/d.py
+++ abc/179/d.py
@@ -1,15 +1,25 @@
 N, K = map(int, input().split())
 
-valid_nums = [False] * (N+1)
+LR = [None] * K
 for i in range(K):
     l, r = map(int, input().split())
-    for j in range(l, r+1):
-        valid_nums[j] = True
+    LR[i] = (l, r)
 
-# dp[i] はマスiに到達する方法の個数
+mod = 998244353
+
 dp = [0] * (N+1)
 dp[1] = 1
 
-for i in range(2, N+1):
-    # dp[i] を埋めていく
-    dp[i]
+cdp = [0] * (N+1)
+cdp[1] = 1
+
+for i in range(1, N+1):
+    for lr in LR:
+        l = max(0, i - lr[1] - 1)
+        r = max(0, i - lr[0])
+        dp[i] += cdp[r] - cdp[l]
+        dp[i] %= mod
+    cdp[i] = dp[i] + cdp[i-1]
+    cdp[i] %= mod
+
+print(dp[N])
